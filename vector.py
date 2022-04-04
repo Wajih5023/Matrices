948f308be295d85f6f9c992dd61bf2f061aedd36
Wajih5023 <wajihnahmed5023@gmail.com>
2022-04-04 18:01:38 -0500

    Added functions and comments

--- vector.py
+++ vector.py
@@ -1,7 +1,9 @@
+# Represents vector
 class Vector ( object ):
+
+  # Creates zero vector of given length
   def __init__ ( self , size = 2 ):
     self.vector = []
-    self.num_of_entries = size
 
     count = 0
     while count < self.num_of_entries:
@@ -9,6 +11,29 @@ class Vector ( object ):
       
       count += 1
   
+  # Creates nonzero vector using given list of entries
+  def createVector ( self , list ):
+    if self.getSize() != len( list ):
+      print( "Given list cannot be used to create vector" )
+
+      return
+    
+    for index in range( self.vector ):
+      self.vector[index] = list[index]
+  
+  # Returns number of entries in vector
+  def getSize ( self ):
+    return len( self.vector )
+  
+  # Checks whether vector is zero vector or not
+  def isZeroVector( self ):
+    for entry in self.vector:
+      if entry != 0:
+        return False
+    
+    return True
+
+  # Prints out vector 
   def __str__ ( self ):
     vec = ""
 
